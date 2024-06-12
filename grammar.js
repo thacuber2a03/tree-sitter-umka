c65413530bc2fd5ee888b6aaf31ee0403911e900
thacuber2a03 <antonidiaz200731@gmail.com>
2024-06-11 20:41:31 -0400

    reformat and abstract optional and repeating sequences

--- grammar.js
+++ grammar.js
@@ -1,210 +1,191 @@
+const implSemi = /[;\n]/
+const optSeq = (...rules) => optional(seq(...rules))
+const repSeq = (...rules) => repeat(seq(...rules))
+
 module.exports = grammar({
-	name: "umka",
+  name: "umka",
 
-	inline: $ => [
-		$.decl,
-		$.stmt,
-		$.expr,
-		$.number,
-		$.varDecl,
-		$.declAssignmentStmt,
-	],
+  inline: $ => [
+    $.decl,
+    $.stmt,
+    $.expr,
+    $.number,
+    $.varDecl,
+    $.declAssignmentStmt,
+  ],
 
-	extras: $ => [
-		$.comment,
-		/\s/,
-	],
+  extras: $ => [
+    $.comment,
+    /\s/,
+  ],
 
-	word: $ => $.ident,
+  word: $ => $.ident,
 
-	conflicts: $ => [
-		[$.fnDecl],
-		[$.methodDecl],
-	],
+  conflicts: $ => [
+    [$.fnDecl],
+    [$.methodDecl],
+  ],
 
-	rules: {
-		program: $ => seq(
-			repeat($.decl)
-		),
+  rules: {
+    program: $ => seq(
+      repeat($.decl)
+    ),
 
-		decl: $ => choice(
-			$.fnDecl,
-			$.methodDecl,
-			$.varDecl,
-		),
+    decl: $ => choice(
+      $.fnDecl,
+      $.methodDecl,
+      $.varDecl,
+      $.typeDecl,
+    ),
 
-		varDecl: $ => choice(
-			$.fullVarDecl,
-			alias($.declAssignmentStmt, $.shortVarDecl)
-		),
+    varDecl: $ => choice(
+      $.fullVarDecl,
+      alias($.declAssignmentStmt, $.shortVarDecl)
+    ),
 
-		fullVarDecl: $ => seq(
-			"var",
-			choice(
-				$.varDeclItem,
-				seq("(", repeat(seq($.varDeclItem, /[\n;]/)), ")"),
-			),
-		),
+    fullVarDecl: $ => seq("var", choice(
+      $.varDeclItem,
+      seq("(", repSeq($.varDeclItem, implSemi), ")"),
+    )),
 
-		varDeclItem: $ => seq(
-			field('identifiers', $.typedIdentList),
-			"=", field('value', $.expr)
-		),
+    varDeclItem: $ => seq(
+      field('identifiers', $.typedIdentList),
+      "=", field('value', $.expr)
+    ),
 
-		identList: $ => seq(
-			$.ident, field('exported', optional('*')),
-			repeat(seq(",", $.ident, field('exported', optional('*')))),
-		),
+    exportMark: $ => '*',
 
-		typedIdentList: $ => seq($.identList, ":", optional(".."), $.type),
+    identList: $ => seq(
+      $.ident, field('exported', optional('*')),
+      repSeq(",", $.ident, field('exported', optional($.exportMark))),
+    ),
 
-    typeDecl: $ => seq(
-      'type',
-      choice($.typeDeclItem, seq("(", repeat($.typeDeclItem), ")"))
+    typedIdentList: $ => seq($.identList, ":", optional(".."), $.type),
+
+    typeDecl: $ => seq('type',
+      choice($.typeDeclItem, seq("(", repSeq($.typeDeclItem, implSemi), ")"))
     ),
 
     typeDeclItem: $ => seq(
-      field('name', $.identifier),
+      field('name', $.ident),
       optional($.exportMark),
-      '=',
-      $.type,
+      '=', $.type,
     ),
 
     type: $ => choice(
       $.qualIdent,
-      $.ptr_type,
-      $.array_type,
+      $.ptrType,
+      $.arrayType,
       $.dynArrayType,
       $.enumType,
-      $.struct_type,
-      $.map_type,
-      $.interface_type,
-      $.closure_type,
+      $.structType,
+      $.mapType,
+      $.interfaceType,
+      $.closureType,
     ),
 
-    ptr_type: $ => seq(
-      optional("weak"),
-      '^',
-      $.type,
-    ),
-
-    array_type: $ => seq(
-      '[',
-      $.expr,
-      ']',
-      $.type,
-    ),
-
+    ptrType: $ => seq(optional("weak"), '^', $.type),
+    arrayType: $ => seq('[', $.expr, ']', $.type),
     dynArrayType: $ => seq('[', ']', $.type),
-
     enumType: $ => seq('enum', '{', repeat($.enumItem), '}'),
-    enumItem: $ => seq(field('name', $.identifier)),
-    structType: $ => seq('struct', '{', repeat(seq($.typedIdentList, /[\n;]/)), '}'),
+    enumItem: $ => seq(field('name', $.ident), implSemi),
+    structType: $ => seq('struct', '{', repSeq($.typedIdentList, implSemi), '}'),
 
     mapType: $ => seq('map', '[', $.type, ']', $.type),
-    interface_type: $ => seq(
-      'interface',
-      '{',
-      repeat($.interface_item),
-      '}',
+    interfaceType: $ => seq('interface', '{', repeat($.interface_item), '}'),
+
+    interfaceItem: $ => choice(
+      field('type', $.ident),
+      seq(field('name', $.ident), $.signature),
     ),
-    interface_item: $ => choice(
-      field('type', $.identifier),
+
+    closureType: $ => seq('fn', $.signature),
+
+    signature: $ => seq($.parameterList, optSeq(':', $.type)),
+
+    declAssignmentStmt: $ => seq(
+      field('identifiers', $.identList),
+      ":=", field('values', $.exprList)
+    ),
+
+    fnDecl: $ => seq("fn",
+      field('name', $.ident),
+      optional(field('exported', '*')),
+      field('signature', $.signature),
+      optional(field('body', $.block)),
+    ),
+
+    methodDecl: $ => seq("fn",
+      field('receiver', $.rcvSignature),
+      field('name', $.ident),
+      optional(field('exported', '*')),
+      field('signature', $.signature),
+      optional(field('body', $.block)),
+    ),
+
+    rcvSignature: $ => seq("(",
+      field('name', $.ident), ":",
+      field('type', $.type),
+    ")"),
+
+    exprList: $ => seq($.expr, repSeq(",", $.expr)),
+
+    signature: $ => seq("(", optSeq(
+      field('params', $.typedIdentList),
+      optSeq('=', field('defaultValue', $.expr)),
+    ), ")"),
+
+    block: $ => seq("{", repSeq($.stmt, optional(';')), "}"),
+
+    stmt: $ => choice(
+      alias($.designator, $.callStmt),
+      $.block,
+      $.decl,
+    ),
+
+    expr: $ => choice($.stringLiteral, $.number),
+
+    designator: $ => seq(choice($.primary)),
+
+    primary: $ => choice($.qualIdent, $.builtinCall),
+
+    qualIdent: $ => seq(
+      optSeq(field('module', $.ident), '::'),
+      field('identifier', $.ident)
+    ),
+
+    builtinCall: $ => seq(
+      field('name', $.qualIdent),
+      field('arguments', seq("(",
+        optSeq($.expr, repSeq(",", $.expr)), ")"
+      ))
+    ),
+
+    ident: $ => /[A-Za-z_][A-Za-z_0-9]*/,
+
+    number: $ => choice($.realNumber, $.hexNumber, $.decNumber),
+
+    decNumber: $ => /[0-9]+/,
+    hexNumber: $ => /[0-9a-fA-F]+/,
+
+    realNumber: $ => choice(
+      /[0-9]+\.[0-9]+/,
+      /[0-9]+[Ee]\-?[0-9]+/,
+      /[0-9]+\.[0-9]+[Ee]\-?[0-9]+/,
+    ),
+
+    charLiteral: $ => seq("'", repeat(choice($.escSeq, /./)), '"'),
+    stringLiteral: $ => seq('"', repeat(choice($.escSeq, /./)), '"'),
+
+    escSeq: $ => choice(/\\[0abefnrtv]/, /\\x[0-9a-fA-F][0-9a-fA-F]*/),
+
+    comment: $ => token(choice(
+      seq('//', /.*/),
       seq(
-        field('name', $.identifier),
-        $.signature
+        '/*',
+        /[^*]*\*+([^/*][^*]*\*+)*/,
+        '/',
       ),
-    ),
-    closure_type: $ => seq(
-      'fn',
-      $.signature,
-    ),
-    signature: $ => seq(
-      $.parameter_list,
-      optional(seq(':', $.type)),
-    ),
-
-		declAssignmentStmt: $ => seq(
-			field('identifiers', $.identList),
-			":=", field('values', $.exprList)
-    ),
-
-		fnDecl: $ => seq("fn",
-			field('name', $.ident),
-			optional(field('exported', '*')),
-			field('signature', $.signature),
-			optional(field('body', $.block)),
-		),
-
-		methodDecl: $ => seq("fn",
-			field('receiver', $.rcvSignature),
-			field('name', $.ident),
-			optional(field('exported', '*')),
-			field('signature', $.signature),
-			optional(field('body', $.block)),
-		),
-
-		rcvSignature: $ => seq("(",
-			field('name', $.ident), ":",
-			field('type', $.type), ")"
-		),
-
-		exprList: $ => seq($.expr, repeat(seq(",", $.expr))),
-
-		signature: $ => seq("(", optional(seq(
-			field('params', $.typedIdentList),
-			optional(seq('=', field('defaultValue', $.expr))),
-		)), ")"),
-
-		block: $ => seq("{", repeat(seq($.stmt, optional(';'))), "}"),
-
-		stmt: $ => choice(
-			alias($.designator, $.callStmt),
-			$.block,
-			$.decl,
-		),
-
-		expr: $ => choice($.stringLiteral, $.number),
-
-		designator: $ => seq(choice($.primary)),
-
-		primary: $ => choice($.qualIdent, $.builtinCall),
-
-		qualIdent: $ => seq(
-			optional(seq(field('module', $.ident), '::')),
-			field('identifier', $.ident)
-		),
-
-		builtinCall: $ => seq(
-			field('name', $.qualIdent),
-			field('arguments', seq("(", optional(seq($.expr, repeat(seq(",", $.expr)))), ")"))
-		),
-
-		ident: $ => /[A-Za-z_][A-Za-z_0-9]*/,
-
-		number: $ => choice($.realNumber, $.hexNumber, $.decNumber),
-
-		decNumber: $ => /[0-9]+/,
-		hexNumber: $ => /[0-9a-fA-F]+/,
-
-		realNumber: $ => choice(
-			/[0-9]+\.[0-9]+/,
-			/[0-9]+[Ee]\-?[0-9]+/,
-			/[0-9]+\.[0-9]+[Ee]\-?[0-9]+/,
-		),
-
-		charLiteral: $ => seq("'", repeat(choice($.escSeq, /./)), '"'),
-		stringLiteral: $ => seq('"', repeat(choice($.escSeq, /./)), '"'),
-
-		escSeq: $ => choice(/\\[0abefnrtv]/, /\\x[0-9a-fA-F][0-9a-fA-F]*/),
-
-		comment: $ => token(choice(
-			seq('//', /.*/),
-			seq(
-				'/*',
-				/[^*]*\*+([^/*][^*]*\*+)*/,
-				'/',
-			),
-		)),
-	}
+    )),
+  }
 })
